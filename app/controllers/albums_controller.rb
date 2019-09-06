eca8e8dbd91b7732d408cb640d6ddf403b269a22
max <maximerieder@yahoo.com>
2019-09-06 15:13:37 +0200

    fix

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,11 @@
 class AlbumsController < ApplicationController
   def index
     @albums = Album.all
-    @channels = Channel.order(rating: :desc).first(4)
+    @channels = Channel.order(rating: :desc).first(5)
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 
   def new
